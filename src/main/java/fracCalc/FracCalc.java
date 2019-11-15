bda5f4e398352e47e1aee4c7092dc9f4bf107f29
s-sinnte <s-sinnte@53S500042369.it.bsd405.org>
2019-11-15 11:54:12 -0800

    Checkpoint 1 EZ$$ Claps

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,29 +4,54 @@
 
 package fracCalc;
 
+import java.util.Scanner;
+
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean done = false;
+		while (!done) {
+			System.out.println("Enter your fractional calculation (Type 0 to quit): ");
+			String input = scan.nextLine();
+			if (input.equals("0")) {
+				break;
+			}
 
-    }
+			System.out.println(produceAnswer(input));
+		}
+		scan.close();
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+		// TODO: Read the input from the user and call produceAnswer with an equation
 
-        return "";
-    }
+	}
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		
+		// TODO: Implement this function to produce the solution to the input
+		
+		Scanner scanInput = new Scanner(input);
+		
+		String fraction1 = scanInput.next();
+		String operator = scanInput.next();
+		String fraction2 = scanInput.next();
+		
+		
+		scanInput.close();
+		return fraction2;
+	}
 
+	// TODO: Fill in the space below with any helper methods that you think you will
+	// need
 }
